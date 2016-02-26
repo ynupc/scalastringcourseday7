48782d8607b579bc373548c20d2f4a08c453dba1
ktr-skmt <sakamoto_911@hotmail.com>
2016-02-26 13:10:52 +0900

    update

--- src/test/scala/text/SentenceParser.scala
+++ src/test/scala/text/SentenceParser.scala
@@ -1,6 +1,9 @@
 package text
 
+import java.nio.file.Paths
+
 import scala.collection.mutable.ListBuffer
+import scala.io.Source
 
 /**
   * @author ynupc
@@ -10,10 +13,22 @@ object SentenceParser {
   private final val japanesePeriodRegex: String = "[。．]"
   private final val japaneseCommaRegex: String  = "[、，]"
   private final val japanesePeriod: String = "。"
+  private final val japanesePeriod2: String = "．"
   private final val japaneseComma: String  = "、"
+
+  private final val basePath: String = "../../src/test/resources/"
+  private final val properNounsWithJapanesePeriod: Seq[String] = initialize()
+  private final val ghost: String = "妛"
+  private final val ghost2: String = "彁"
 
   // このようなprivateな型を作成しておかないとobject SentenceParser内のprivate class NormalizedSentenceをSeqで返すparseメソッドが作成ができない。
   private type NS = NormalizedSentence
+
+  private def initialize(): Seq[String] = {
+    Source.fromFile(
+      Paths.get(basePath, "proper_noun_with_japanese_period.txt").toFile
+    ).getLines().toSeq.sortWith((a, b) => a.length > b.length)
+  }
 
   def parse(textOpt: StringOption): Seq[NS] = {
     if (textOpt.isEmpty) {
@@ -24,12 +39,25 @@ object SentenceParser {
 
     //改行文字により行に分割
     textOpt.get split '\n' foreach {
-      line =>
+      l =>
+        var line: String = l
         sentences ++= {
+          for (properNoun <- properNounsWithJapanesePeriod if line contains properNoun) {
+            line = line.replaceAllLiterally(
+              properNoun,
+              properNoun.
+                replaceAllLiterally(japanesePeriod, ghost).
+                replaceAllLiterally(japanesePeriod2, ghost2)
+            )
+          }
+
           //句点により文単位に分割
           for (sentence <- line.trim split japanesePeriodRegex
                if StringOption(sentence).nonEmpty) yield {
-            new NormalizedSentence(parseSentence(sentence))
+            new NormalizedSentence(parseSentence(sentence).
+              replaceAllLiterally(ghost, japanesePeriod).
+              replaceAllLiterally(ghost2, japanesePeriod2)
+            )
           }
         }
     }
